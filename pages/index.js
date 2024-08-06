f5bc890ad3173e85d163a91ae49deba0c527d7f1
Thyrson Resquin <61873064+thyrsonresquin@users.noreply.github.com>
2024-08-06 14:20:29 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Jesiel, quer faltar academia para ir comer hoje, em comemoração do novo emprego... Bjo! (responda mandando uma figurinha no whatsapp)</h1>
+}
+
+export default Home;
