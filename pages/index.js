96511b9aabd43d9cbd371ee8c99cdcfa53236354
Thyrson Resquin <61873064+thyrsonresquin@users.noreply.github.com>
2024-08-06 15:54:51 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Jesiel, quer faltar academia para ir comer hoje, em comemoração do novo emprego... Bjo! (responda mandando uma figurinha no whatsapp)</h1>
+    return <h1>Jesiel &#128540;, quer faltar academia para ir comer hoje, em comemoração do novo emprego... Bjo! (responda mandando uma figurinha no whatsapp)</h1>
 }
 
 export default Home;
