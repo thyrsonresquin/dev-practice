083ae000999a08df736f08538f49a937f559e7cb
Thyrson Resquin <61873064+thyrsonresquin@users.noreply.github.com>
2024-08-14 20:40:55 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Jesiel &#128540;, quer faltar academia para ir comer hoje, em comemoração do novo emprego... Bjo! (responda mandando uma figurinha no whatsapp)</h1>
+  return (
+    <h1>
+      Jesiel &#128540;, quer faltar academia para ir comer hoje, em comemoração
+      do novo emprego... Bjo! (responda mandando uma figurinha no whatsapp)
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
